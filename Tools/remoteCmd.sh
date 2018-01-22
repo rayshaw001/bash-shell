4b2d754a968725cf128e501478a3b8912feabe47
xiaorui <rxiao@hpe.com>
2018-01-22 16:55:59 +0800

    run command on remote host

---
+++ Tools/remoteCmd.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+function remoteCmd(){
+        sshpass -p $passwd ssh $uname@$node -- $cmd
+        echo "remote command: sshpass -p $passwd ssh $uname@$node -- $cmd complete"
+}
+if [ $# > 3 ]; then
+node=$1
+uname=$2
+passwd=$3
+cmd=$4
+remoteCmd $node $uname $passwd $cmd
+else
+echo -e 'missing one or more of following argument(s):' \
+        '\n node        ------  hostname or ip'  \
+        '\n uname       ------  username '  \
+        '\n passwd      ------  password '  \
+        '\n cmd         ------  command that you want to run on remote host\n';
+fi
